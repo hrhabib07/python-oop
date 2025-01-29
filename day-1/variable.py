f3df1f721cffb1461a382cdec8353932044fce8c
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2025-01-29 21:44:16 +0600

    variable explored in python

---
+++ day-1/variable.py
@@ -0,0 +1,21 @@
+# name = value
+age = 21
+favorite_number=3.1416
+name="Abul Uddin"
+is_single=True
+varsity='Metropolitan University'
+
+print(varsity)
+print(name)
+
+print(type(age))
+print(type(favorite_number))
+print(type(is_single))
+ 
+""" 
+to write multiline comment 
+
+    alt + shift + a
+
+
+"""
