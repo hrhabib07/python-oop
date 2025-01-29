a2b4905f0702beba9feb9caa961d30ca9fd20c7e
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2025-01-29 22:54:42 +0600

    coditinals and operator explred

---
+++ day-1/conditional.py
@@ -0,0 +1,19 @@
+a=55
+b=100
+if a>b :
+    print(f"{a} is grater than {b}")
+elif a>9 :
+    print(f"{a} is grater than 9")
+else : 
+    print(f"{a} is less than {b}")
+
+boss=False
+if boss is not True :
+    print("luncher pore ashen")
+else: 
+    print("thaler bxo nye asi")
+
+""" if boss is True :
+    print("teler baxo niye astesi")
+else: 
+    print("luncher proe asen") """
---
+++ day-1/operators.py
@@ -0,0 +1,6 @@
+english_marks=56;
+math_marks=77;
+avg=(english_marks+math_marks)/2;
+avg_int=(english_marks+math_marks)//2;
+print(avg)
+print(avg_int)
